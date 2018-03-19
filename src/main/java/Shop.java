9d530e5f8d2a1ab5366f6a494f63eb98cc1c60d8
Oli <oliver.berry84@gmail.com>
2018-03-19 09:45:36 +0000

    tony push

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -2,30 +2,33 @@ import Interfaces.ISell;
 
 import java.util.ArrayList;
 
-public class Shop implements ISell{
+public class Shop implements ISell {
 
     private String name;
     private int buyPrice;
     private int sellPrice;
     private ArrayList<ISell> stock;
 
-    public Shop(String name, int buyPrice, int sellPrice){
+    public Shop(String name, int buyPrice, int sellPrice) {
         this.name = name;
         this.buyPrice = buyPrice;
         this.sellPrice = sellPrice;
         this.stock = new ArrayList<ISell>();
     }
 
-    public void calculateMarkUp(ISell stock){
-        this.stock.add(stock);
-    }
-
-    public void calculateMarkUp(ISell stock){
-        this.stock.remove(stock);
-    }
-
     @Override
     public int calculateMarkUp(int buyPrice, int sellPrice) {
         return this.sellPrice - this.buyPrice;
+    } // for loop for array?
+
+
+    public void calculateMarkUp(ISell stock) {
+        this.stock.add(stock);
     }
+
+    public void calculateMarkUp(ISell stock) {
+        this.stock.remove(stock);
+    }
+
+
 }
